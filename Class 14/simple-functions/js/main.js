4bcc13fdf29cf352f835368897dab96f3c35f1ce
Macir <mj.brustolin@hotmail.com>
2022-03-01 23:11:38 -0300

    Class 15 - Class Materials (Freelancing)

--- Class 14/simple-functions/js/main.js
+++ Class 14/simple-functions/js/main.js
@@ -1,5 +1,15 @@
 //create a function that adds two numbers and alerts the sum
-
+function add(param1, param2)
+{
+    alert(param1 + param2)
+} 
 //create a function that multiplys three numbers and console logs the product
-
+function multiply(param1, param2, param3)
+{
+    console.log(param1 * param2 * param3)
+}
 //create a function that divides two numbers and returns the ???
+function divide(param1, param2)
+{
+    return param1 / param2
+}
