a35a03a752b6d3ee346cf92ac1ce6d3584ad71cf
Macir <mj.brustolin@hotmail.com>
2022-03-06 17:45:11 -0300

    Class 16 - Office Hours (Js Bring it On and Bring it on Again)

--- Class 16/bring-it-on-again/js/main.js
+++ Class 16/bring-it-on-again/js/main.js
@@ -1,17 +1,43 @@
 // *Variables*
 // Declare a variable, assign it a value, and alert the value
-
+let variable = 5
+console.log(variable)
 // Create a variable, divide it by 10, and console log the value
+let variable2 = 6
+console.log(variable2 / 10)
 
 // *Functions*
 // Create a function that multiplys 3 numbers and alerts the product
-
+function Mult3NumbsAndAlert(n1, n2, n3){
+    alert(n1 * n2 * n3)
+}
 // Create a function that takes in 4 numbers. Add the first two numbers and subtract the next two. Console log the result
+function AddFirst2NumbsAndSubLast2(n1, n2, n3, n4){
+    console.log(n1 + n2 - n3 - n4)
+}
 
 // *Conditionals*
 // Create a function that takes in 3 numbers. Starting with 100 add the first number, subtract the second, and divide the third. If the value is greater then 25, console log "WE HAVE A WINNNA"
-
+function Take3NumbsAndVerify(n1, n2, n3){
+    if ((100 + n1 - n2) / n3 > 25) {
+        console.log("We Have A Winna!!!")
+    }
+}
 // Create a function that takes in a day of the week. If it is a weekend alert, "weekend" and if not alert "week day". Handle capitilization and if the user does not enter a day of the week alert "Try again!"
-
+function DayOfWeekCheck(dayOfWeek){
+    dayOfWeekLowerCase = dayOfWeek.toLowerCase()
+    if (dayOfWeek === "saturday" || dayOfWeek === "sunday"){
+        alert("Weekend!!!")
+    } else if (dayOfWeek === "monday" || dayOfWeek === "tuesday" || dayOfWeek === "wednesday" || dayOfWeek === "thursday" || dayOfWeek === "friday"){
+        alert("Week Day :(")
+    } else {
+        alert("Try Again!")
+    }
+}
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number or greater, but count by 3
+function LogsNumbersByThree(n1){
+    for (let i = 1; i <= n1; i += 3) {
+        console.Log(i)
+    }
+}
--- Class 16/bring-it-on/js/main.js
+++ Class 16/bring-it-on/js/main.js
@@ -1,17 +1,50 @@
 // *Variables*
 // Create a variable and console log the value
-
+let num = 7
+console.log(num)
 // Create a variable, add 10 to it, and alert the value
+let otherNum = 5
+otherNum += 10
+alert(otherNum)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function Sub4NumbersAndAlert(n1, n2, n3, n4){
+    const sub4nums = n1 - n2 - n3 - n4
+    alert(sub4nums)
+}
+Sub4NumbersAndAlert(100, 20, 10, 5)
 // Create a function that divides one number by another and returns the remainder
+function Divide2NumsAndReturn(n1, n2){
+    return n1 % n2
+}
+alert(Divide2NumsAndReturn(10, 5))
 
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-
+function Add2NumsAndAlert(n1, n2){
+    if (n1 + n2 > 50){
+        alert("Jumanji")
+    }
+}
+Add2NumsAndAlert(25, 26)
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+function Mult3NumbAlert(n1, n2, n3){
+    if ((n1 * n2 * n3) % 3 === 0){
+        alert("Zebra")
+    }
+}
+Mult3NumbAlert(3, 3, 3)
 
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+const userInputWord = prompt()
+const userInputNumber = prompt()
+
+function ConsoleWordXTimes(word, number){
+    for(let i = 0; i < number; i++){
+        console.log(word)
+    }
+}
+
+ConsoleWordXTimes(userInputWord, userInputNumber)
